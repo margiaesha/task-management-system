03d66fe7e9982b8c7f28cabe901a2d62c6f825ce
Margia Sultana <marjiasultana55@email.com>
2023-05-11 16:33:28 +0600

    new commit code

--- src/Componenets/Login/Signup/Signup.js
+++ src/Componenets/Login/Signup/Signup.js
@@ -10,12 +10,12 @@ const Signup = () => {
 
       
         <div>
-            <h2 className='text-4xl'>Sign Up</h2>
+            <h2 className='text-4xl my-4'>Sign Up</h2>
             <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
     
    
-    <input {...register("firstName")}  type="text"  placeholder="First name" className="input input-bordered input-primary w-full max-w-xs" />
-    <select {...register("category", { required: true })} className="select select-primary w-full max-w-xs">
+    <input {...register("firstName")}  type="text"  placeholder="First name" className="input input-bordered input-primary w-full max-w-xs my-4"  />
+    <select {...register("category", { required: true })} className="select select-primary w-full max-w-xs my-4">
   <option disabled selected value="">Department</option>
   <option value="A">Web Development</option>
   <option value="B">Content</option>
@@ -23,14 +23,14 @@ const Signup = () => {
   <option value="D">Email Marketing</option>
   <option value="E"> PPC</option>
   <option value="F">HR</option>
-  <option value="G">Graphix</option>
+  <option value="G">Graphics</option>
   <option value="H">UI/UX</option>
 </select>
  
     <p>{data}</p>
    
   
-    <button type="submit" className="btn btn-wide btn-primary ">Submit</button>
+    <button type="submit" className="btn btn-wide btn-primary my-4">Submit</button>
 
   </form>
         </div>
